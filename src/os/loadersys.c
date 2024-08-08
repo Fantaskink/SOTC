81265e991f0671d2ab9a0f1230ddd3a24c10dc3c
Fantaskink <johan.riedel@gmail.com>
2024-08-08 16:14:05 +0200

    Rename loadersys -> loaderSys

--- src/os/loadersys.c
+++ src/os/loadersys.c
@@ -1,150 +1,8 @@
 #include "common.h"
-#include "sdk/ee/eekernel.h"
-#include "sdk/ee/sifdev.h"
-#include "gcc/string.h"
-#include "fl_xfftype.h"
-
-extern char *D_00131DC0[]; // {{"normal use"}, {"\x1B[36mout of align(alloc)\x1B[m"}, {"alloc flag(alloc)"}}
-extern char D_00136318[];  // "ld:\t\tdecode section\n"
-extern char D_00136330[];  // "ld:\t%15s(progbit): 0x%08x(0x%08x) %s\n"
-extern char D_00136358[];  // "ld:\t%15s(overlaydata): 0x%08x(0x%08x) %s\n"
-extern char D_00136388[];  // "ld:\t%15s(nobit)  : 0x%08x(0x%08x) %s\n"
-
-extern u32 D_00139F04; // heap pointer
-
-#define HEAP_START D_00139F04
-
-#define SEMAPHORE_LIST D_0013BD10
-#define THREAD_LIST D_0013B910
-#define IOP_MEMORY_LIST D_0013C910
-#define INTC_HANDLER_LIST D_0013C110
-
-#define MAX_INTC_HANDLERS 256
-#define IOP_MEM_LIST_LEN 256
-
-extern s32 D_0013BD10[MAX_SEMAPHORES];
-extern s32 D_0013B910[MAX_THREADS];
-extern s32 D_0013C910[IOP_MEM_LIST_LEN];
-extern struct unk D_0013C110[MAX_INTC_HANDLERS];
-
-extern char D_0013A100[]; // "host0:"
-
-extern const char D_0013D110[]; // Filled at runtime: "cdrom0:\SCPS_15"
-extern const char D_0013A0F8[]; // "%s"
-
-struct unk
-{
-    s32 unk0;
-    s32 unk4;
-};
-
-// Function prototypes
-extern s32 printf(const char *string, ...);
 
 INCLUDE_ASM(const s32, "os/loadersys", ResolveRelocation);
 
-void DecodeSection(
-    void *xffBuf,
-    void *(*mallocAlign)(int sz, int align),
-    void *(*mallocMaxAlign)(int sz),
-    void (*ldrDbgPrintf)(char *fmt, ...))
-{
-    int i;
-    struct t_xffEntPntHdr *xffEp;
-    void *entPntSectBs = NULL;
-    struct t_xffSsNmOffs *nmOffs;
-    struct t_xffSectEnt *sect;
-
-    xffEp = xffBuf;
-
-    i = xffEp->sectNrE - 1;
-    sect = &xffEp->sectTab[1];
-    nmOffs = &xffEp->ssNamesOffs[0];
-
-    if (ldrDbgPrintf != NULL)
-        ldrDbgPrintf(GSTR(D_00136318, "ld:\t\tdecode section\n"));
-
-    // The zero section is not processed as it is all 0.
-    for (; i--; sect++, nmOffs++)
-    {
-
-        sect->moved = 0;
-        if (sect->size != 0)
-        {
-            switch (sect->type)
-            {
-            case 1:
-            case 0x7FFFF420: // .text section
-                if (sect->flags != 0)
-                {
-                    // Forced max alignment
-                    sect->memPt = mallocMaxAlign(sect->size);
-                    memcpy(sect->memPt, sect->filePt, sect->size);
-                    sect->moved = 2;
-                }
-                else
-                {
-                    // Check if the alignment is enough as is in the file:
-                    if (((u32)sect->filePt & (sect->align - 1)) != 0)
-                    {
-                        // Insufficient alignment, so alloicate
-                        sect->memPt = mallocAlign(sect->size, sect->align);
-                        memcpy(sect->memPt, sect->filePt, sect->size);
-                        sect->moved = 1;
-                    }
-                    else
-                    { // use the section as is in the file
-                        sect->memPt = sect->filePt;
-                    }
-                }
-                if (ldrDbgPrintf != NULL)
-                {
-                    if (sect->type == 1)
-                    { // .text
-                        // The moved-types are actually allocation types (or used in file as is).
-                        ldrDbgPrintf(GSTR(D_00136330, "ld:\t%15s(progbit): 0x%08x(0x%08x) %s\n"), &xffEp->ssNamesBase[nmOffs->nmOffs], sect->memPt, sect->size, D_00131DC0[sect->moved]);
-                    }
-                    else
-                    { // VU code/data
-                        // The moved-types are actually allocation types (or used in file as is).
-                        ldrDbgPrintf(GSTR(D_00136358, "ld:\t%15s(overlaydata): 0x%08x(0x%08x) %s\n"), &xffEp->ssNamesBase[nmOffs->nmOffs], sect->memPt, sect->size, D_00131DC0[sect->moved]);
-                    }
-                }
-                break;
-            case 8: // nobit .bss section
-                // Because nobits is not present in the file, it is always allocated
-                if (sect->flags != 0)
-                { // section is not copied and used as is in the file
-                    sect->memPt = mallocMaxAlign(sect->size);
-                    sect->moved = 2;
-                }
-                else
-                {
-                    sect->memPt = mallocAlign(sect->size, sect->align);
-                    sect->moved = 1;
-                }
-
-                memset(sect->memPt, 0x00, sect->size);
-                if (ldrDbgPrintf != NULL)
-                {
-                    // The moved-types are actually allocation types (or used in file as is).
-                    ldrDbgPrintf(GSTR(D_00136388, "ld:\t%15s(nobit)  : 0x%08x(0x%08x) %s\n"), &xffEp->ssNamesBase[nmOffs->nmOffs], sect->memPt, sect->size, D_00131DC0[sect->moved]);
-                }
-                break;
-            }
-        }
-        else
-        {
-            sect->memPt = NULL;
-        }
-
-        if (entPntSectBs == NULL)
-            entPntSectBs = sect->memPt;
-    }
-
-    xffEp->entryPnt = (void *)((u32)entPntSectBs + xffEp->entryPnt_Rel);
-    return;
-}
+INCLUDE_ASM(const s32, "os/loadersys", DecodeSection);
 
 INCLUDE_ASM(const s32, "os/loadersys", RelocateElfInfoHeader);
 
@@ -172,15 +30,9 @@ INCLUDE_ASM(const s32, "os/loadersys", RelocateSelfSymbol);
 
 INCLUDE_ASM(const s32, "os/loadersys", DisposeRelocationElement);
 
-void SetHeapStartPoint(u32 start_address)
-{
-    HEAP_START = (start_address + 0xF) & ~0xF;
-}
+INCLUDE_ASM(const s32, "os/loadersys", SetHeapStartPoint);
 
-int GetHeapCurrentPoint(void)
-{
-    return HEAP_START;
-}
+INCLUDE_ASM(const s32, "os/loadersys", GetHeapCurrentPoint);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysResetSystem);
 
@@ -188,21 +40,7 @@ INCLUDE_ASM(const s32, "os/loadersys", mallocAlignMempool);
 
 INCLUDE_ASM(const s32, "os/loadersys", mallocAlign0x100Mempool);
 
-s32 _checkExistString(char *string, char **strings)
-{
-    s32 i = 0;
-
-    while (strings[i] != 0)
-    {
-        if (strcmp(strings[i], string) == 0)
-        {
-            return 1;
-        }
-        i++;
-    }
-
-    return 0;
-}
+INCLUDE_ASM(const s32, "os/loadersys", _checkExistString);
 
 INCLUDE_ASM(const s32, "os/loadersys", func_00101B88);
 
@@ -226,310 +64,81 @@ INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteAllExternalSema);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysExecuteRecoveryFirstProcess);
 
-static inline s32 getStA()
-{
-    s32 i;
-    for (i = 0; i < MAX_INTC_HANDLERS; i++)
-    {
-        if (INTC_HANDLER_LIST[i].unk4 < 0)
-            return i;
-    }
-    return -1;
-}
-
-void LoaderSysEntryExternalIntcHandlerList(s32 param_1, s32 param_2)
-{
-    s32 i = getStA();
-    INTC_HANDLER_LIST[i].unk4 = param_1;
-    INTC_HANDLER_LIST[i].unk0 = param_2;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysEntryExternalIntcHandlerList);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysEntryExternalThreadList);
 
-static inline int FindSemaIndex(void)
-{
-    int i;
-    for (i = 0; i < MAX_SEMAPHORES; i++)
-    {
-        if (SEMAPHORE_LIST[i] < 0)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-void LoaderSysEntryExternalSemaList(s32 arg1)
-{
-    SEMAPHORE_LIST[FindSemaIndex()] = arg1;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysEntryExternalSemaList);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysEntryExternalIopMemoryList);
 
-s32 LoaderSysDeleteExternalIntcHandlerList(s32 arg)
-{
-    s32 i;
-    for (i = 0; i < MAX_INTC_HANDLERS; i++)
-    {
-        if (INTC_HANDLER_LIST[i].unk4 == arg)
-        {
-            INTC_HANDLER_LIST[i].unk0 = INTC_HANDLER_LIST[i].unk4 = -1;
-            return arg;
-        }
-    }
-    return -1;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteExternalIntcHandlerList);
 
-s32 LoaderSysDeleteExternalThreadList(s32 threadId)
-{
-    s32 i;
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteExternalThreadList);
 
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if (THREAD_LIST[i] == threadId)
-        {
-            THREAD_LIST[i] = -1;
-            return threadId;
-        }
-    }
-    return -1;
-}
-
-s32 LoaderSysDeleteExternalSemaList(s32 semaId)
-{
-    s32 i;
-    for (i = 0; i < MAX_SEMAPHORES; i++)
-    {
-        if (SEMAPHORE_LIST[i] == semaId)
-        {
-            SEMAPHORE_LIST[i] = -1;
-            return semaId;
-        }
-    }
-    return -1;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteExternalSemaList);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteExternalIopMemoryList);
 
-void LoaderSysInitExternalIntcHandlerList(void)
-{
-    int i;
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysInitExternalIntcHandlerList);
 
-    for (i = 0; i < MAX_INTC_HANDLERS; i++)
-    {
-        INTC_HANDLER_LIST[i].unk0 = INTC_HANDLER_LIST[i].unk4 = -1;
-    }
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysInitExternalSemaList);
 
-void LoaderSysInitExternalSemaList(void)
-{
-    s32 i;
-    for (i = 0; i < MAX_SEMAPHORES; i++)
-    {
-        SEMAPHORE_LIST[i] = -1;
-    }
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysInitExternalThreadList);
 
-void LoaderSysInitExternalThreadList(void)
-{
-    s32 i;
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        THREAD_LIST[i] = -1;
-    }
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysExternalThreadListCallBack);
 
-void LoaderSysExternalThreadListCallBack(void (*fun_ptr)(int))
-{
-    s32 i;
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if (THREAD_LIST[i] != -1)
-        {
-            (*fun_ptr)(THREAD_LIST[i]);
-        }
-    }
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysChangeExternalThreadPriorityExceptMe);
 
-void LoaderSysChangeExternalThreadPriorityExceptMe(s32 priority)
-{
-    s32 i;
-    s32 threadId;
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteAllExternalThread);
 
-    threadId = GetThreadId();
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if ((THREAD_LIST[i] != threadId) && (-1 < THREAD_LIST[i]))
-        {
-            ChangeThreadPriority(THREAD_LIST[i], priority);
-        }
-    }
-    return;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteAllExternalThreadExceptMe);
 
-void LoaderSysDeleteAllExternalThread(void)
-{
-    s32 i;
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysInitExternalIopMemoryList);
 
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if (THREAD_LIST[i] >= 0)
-        {
-            TerminateThread(THREAD_LIST[i]);
-            DeleteThread(THREAD_LIST[i]);
-            THREAD_LIST[i] = -1;
-        }
-    }
-}
-
-void LoaderSysDeleteAllExternalThreadExceptMe(void)
-{
-    s32 threadId;
-    s32 i;
-
-    threadId = GetThreadId();
-
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if ((THREAD_LIST[i] != threadId) && (THREAD_LIST[i] >= 0))
-        {
-            TerminateThread(THREAD_LIST[i]);
-            DeleteThread(THREAD_LIST[i]);
-            THREAD_LIST[i] = -1;
-        }
-    }
-}
-
-void LoaderSysInitExternalIopMemoryList(void)
-{
-    s32 i;
-
-    for (i = 0; i < IOP_MEM_LIST_LEN; i++)
-    {
-        IOP_MEMORY_LIST[i] = 0;
-    }
-}
-
-void LoaderSysDeleteAllExternalIopMemory(void)
-{
-    s32 i;
-
-    sceSifInitRpc(0);
-    sceSifInitIopHeap();
-
-    for (i = 0; i < IOP_MEM_LIST_LEN; i++)
-    {
-        if (IOP_MEMORY_LIST[i] != 0)
-        {
-            sceSifFreeIopHeap((void *)IOP_MEMORY_LIST[i]);
-            IOP_MEMORY_LIST[i] = 0;
-        }
-    }
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDeleteAllExternalIopMemory);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysPrintf);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysLoadIopModuleFromEEBuffer);
 
-s32 LoaderSysCheckCDBootMode()
-{
-    return 2;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysCheckCDBootMode);
 
-void LoaderSysPutString(char *string)
-{
-    printf(GSTR(D_0013A0F8, "%s"), string);
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysPutString);
 
-s32 LoaderSysFOpen(const char *name, s32 flags)
-{
-    s32 success = sceOpen(name, flags);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysFOpen);
 
-s32 LoaderSysFClose(s32 fd)
-{
-    s32 success = sceClose(fd);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysFClose);
 
-s32 LoaderSysFSeek(s32 fd, s32 offset, s32 whence)
-{
-    s32 success = sceLseek(fd, offset, whence);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysFSeek);
 
-s64 LoaderSysFSeek64(s32 fd, s64 offset, s32 whence)
-{
-    s64 success = sceLseek64(fd, offset, whence);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysFSeek64);
 
-s32 LoaderSysFRead(s32 fd, void *buf, s32 count)
-{
-    s32 success = sceRead(fd, buf, count);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysFRead);
 
-s32 LoaderSysFWrite(s32 fd, const void *buf, s32 count)
-{
-    s32 success = sceWrite(fd, buf, count);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysFWrite);
 
-s32 LoaderSysRemove(const char *name)
-{
-    s32 success = sceRemove(name);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysRemove);
 
-s32 LoaderSysRmdir(const char *name)
-{
-    s32 success = sceRmdir(name);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysRmdir);
 
-s32 LoaderSysMkdir(u8 *name, s32 mode)
-{
-    s32 success = sceMkdir(name, mode);
-    return success;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysMkdir);
 
-s32 LoaderSysGetstat(const char *name, struct sce_stat *buf)
-{
-    return sceGetstat(name, buf);
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysGetstat);
 
-s32 LoaderSysChstat(const char *name, struct sce_stat *buf, u32 cbit)
-{
-    return sceChstat(name, buf, cbit);
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysChstat);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysGetMemoryInfo);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysDumpIopModuleIdentifiers);
 
-void LoaderSysSendAbort(void)
-{
+void LoaderSysSendAbort(void) {
 }
 
-void LoaderSysFlushPrint(void)
-{
+void LoaderSysFlushPrint(void) {
 }
 
-char *checkHookDesc(char *hook_desc)
-{
-    int hasDiff;
-    char *desc;
-
-    hasDiff = strncmp(hook_desc, GSTR(D_0013A100, "host0:"), 6);
-    desc = hook_desc + 6;
-    if (hasDiff == 0)
-    {
-        return desc;
-    }
-    return NULL;
-}
+INCLUDE_ASM(const s32, "os/loadersys", checkHookDesc);
 
 INCLUDE_ASM(const s32, "os/loadersys", setNewIopIdentifier);
 
@@ -539,17 +148,13 @@ INCLUDE_ASM(const s32, "os/loadersys", loaderLoop);
 
 INCLUDE_ASM(const s32, "os/loadersys", main);
 
-const char *LoaderSysGetBootArg(void)
-{
-    return D_0013D110;
-}
+INCLUDE_ASM(const s32, "os/loadersys", LoaderSysGetBootArg);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysLoadIopModule);
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysUnloadIopModuleByName);
 
-void LoaderSysHookPoint(void)
-{
+void LoaderSysHookPoint(void) {
 }
 
 INCLUDE_ASM(const s32, "os/loadersys", LoaderSysRebootIop);
@@ -562,8 +167,7 @@ INCLUDE_ASM(const s32, "os/loadersys", memprintf);
 
 INCLUDE_ASM(const s32, "os/loadersys", imemprintf);
 
-void initmemprintf(void)
-{
+void initmemprintf(void) {
 }
 
 INCLUDE_ASM(const s32, "os/loadersys", func_00104090);
