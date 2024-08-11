244f5f10248316b98a67619f60bdbed3b9caa1ab
Fantaskink <johan.riedel@gmail.com>
2024-08-11 18:35:02 +0200

    Delete source files

--- src/os/powerOff.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/nonmatchings/os/powerOff", LoaderSysSetPowerOffCallBackFunc);
-
-INCLUDE_ASM("asm/nonmatchings/os/powerOff", PreparePowerOff);
-
-INCLUDE_ASM("asm/nonmatchings/os/powerOff", func_00104FF0);
--- src/os/usbSerialSys.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", usbSerialSysPutString);
-
-INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", usbSerialSysPrintf);
-
-INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", usbSerialSysInit);
-
-INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", func_001053F0);
