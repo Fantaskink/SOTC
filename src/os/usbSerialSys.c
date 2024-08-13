88c1c22ca2b411b6e86ae2186b95557d9189187c
Fantaskink <johan.riedel@gmail.com>
2024-08-14 00:27:06 +0200

    Reverse usbSerialSysPrintf
    By Wisi

--- src/os/usbSerialSys.c
+++ src/os/usbSerialSys.c
@@ -1,8 +1,21 @@
 #include "common.h"
+#include "usbSerialSys.h"
 
 INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", usbSerialSysPutString);
 
-INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", usbSerialSysPrintf);
+s32 usbSerialSysPrintf(char *format, ...)
+{
+    char buffer[0x100] __attribute__((aligned(16)));
+    s32 ret;
+    va_list args;
+    va_start(args, format);
+
+    vsprintf(buffer, format, args);
+    ret = usbSerialSysPutString(buffer);
+
+    va_end(args);
+    return ret;
+}
 
 INCLUDE_ASM("asm/nonmatchings/os/usbSerialSys", usbSerialSysInit);
 
