86092275869dde69a8037f65fd2a407beef2fdd9
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2025-03-06 18:42:21 -0500

    Implement PowerOffHandler

--- src/os/powerOff.c
+++ src/os/powerOff.c
@@ -1,7 +1,15 @@
 #include "common.h"
+#include "sdk/ee/eekernel.h"
+
+extern s32 D_0013A218;
 
 INCLUDE_ASM("asm/nonmatchings/os/powerOff", LoaderSysSetPowerOffCallBackFunc);
 
 INCLUDE_ASM("asm/nonmatchings/os/powerOff", PreparePowerOff);
 
-INCLUDE_ASM("asm/nonmatchings/os/powerOff", func_00104FF0);
+INCLUDE_ASM("asm/nonmatchings/os/powerOff", PowerOffThread);
+
+static void PowerOffHandler(void *arg)
+{
+    iSignalSema(D_0013A218);
+}
