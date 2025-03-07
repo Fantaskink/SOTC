86ecced294f780e07af482cca2a255953d1ee9a9
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2025-03-07 01:40:57 -0500

    Run formatter on usbSerialSys

--- include/usbSerialSys.h
+++ include/usbSerialSys.h
@@ -9,13 +9,12 @@ int usbSerialSysPrintf(char *format, ...);
 
 int usbSerialSysInit(void);
 
-static void usbSerialSysISignalSema(void* arg0);
+static void usbSerialSysISignalSema(void *arg0);
 
-
-//Shared with the IOP module - should really be in a common include for both, together with the transfer buffers sizes.
-#define USB_SERIAL_BIND_ID			0x80000220	//REQ_ID
-#define USB_SERIAL_FUNC_SEND		0x10			//RPC func ID
-#define USB_SERIAL_BUF_SEND_SZ	0x40			//In bytes.
-#define USB_SERIAL_BUF_RECV_SZ	0x20
+// Shared with the IOP module - should really be in a common include for both, together with the transfer buffers sizes.
+#define USB_SERIAL_BIND_ID 0x80000220 // REQ_ID
+#define USB_SERIAL_FUNC_SEND 0x10     // RPC func ID
+#define USB_SERIAL_BUF_SEND_SZ 0x40   // In bytes.
+#define USB_SERIAL_BUF_RECV_SZ 0x20
 
 #endif /* _USBSERIALSYS_H_ */
