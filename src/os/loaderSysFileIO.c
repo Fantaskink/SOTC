07a54dc483acc57a30e13672591809dbafcc046c
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2025-03-13 10:07:10 -0500

    Match FileIO at -O3

--- src/os/loaderSysFileIO.c
+++ src/os/loaderSysFileIO.c
@@ -5,39 +5,35 @@
 
 #define MAX_IOP_IDENTIFIERS 64
 
-extern s32 D_0013A108;                           // number of iop modules
+s32 D_0013A108 = 0;                           // number of iop modules
 extern char D_0013CD10[MAX_IOP_IDENTIFIERS][16]; // iop module identifiers
 
-extern s32 D_0013A110;
-extern s32 D_0013A184;
-extern s32 D_0013A114;
-
-extern char D_0013A100[]; // "host0:"
-extern char D_00136A80[]; // "ldsys: setNewIopIdentifier: set new iop identifier \"%s\" at #%d\n", Referenced in LoaderSysLoadIopModuleFromEEBuffer
-
 extern struct unk_00131D00_s D_00131D00;
-
-extern int D_0013A180;
-extern int D_0013A184;
-
-static inline void __inlined_setNewIopIdentifier(const char *newIdentifier)
-{
-    LoaderSysPrintf(D_00136A80, newIdentifier, D_0013A108);
-    strncpy(D_0013CD10[D_0013A108++], newIdentifier, strlen(newIdentifier));
-}
+extern s32 D_0013A110;
+extern s32 D_0013A114;
+extern s32 D_00139F00;
 
 s32 LoaderSysPrintf(const char *format, ...)
 {
-    va_list args;
     char buffer[256];
-
+    va_list args;
+    
     va_start(args, format);
     vsprintf(buffer, format, args);
     va_end(args);
-
+    
     printf(buffer);
-
+    
     return strlen(buffer);
+}
+
+void LoaderSysPutString(char *string)
+{
+    printf("%s", string);
+}
+
+void LoaderSysFlushPrint(void)
+{
 }
 
 static inline s32 LoaderSysSearchInLoadedIopModules(const char *module_name)
@@ -53,8 +49,6 @@ static inline s32 LoaderSysSearchInLoadedIopModules(const char *module_name)
     return -1;
 }
 
-extern const char D_00136A00[];
-extern const char D_00136A48[];
 static inline s32 _loadIopModuleFromEEBuffer(s32 data, s32 module_size, s32 argc, const char *argp)
 {
     s32 stat;
@@ -68,7 +62,7 @@ static inline s32 _loadIopModuleFromEEBuffer(s32 data, s32 module_size, s32 argc
     sceSifInitIopHeap();
 
     module_addr = sceSifAllocSysMemory(0x1, module_size, NULL);
-    LoaderSysPrintf(GSTR(D_00136A00, "ldsys: _loadIopModuleFromEEBuffer: allocated iop memory:%p(size:%d)\n"), module_addr, module_size);
+    LoaderSysPrintf("ldsys: _loadIopModuleFromEEBuffer: allocated iop memory:%p(size:%d)\n", module_addr, module_size);
     FlushCache(0x0);
     dma_data.addr = (int)module_addr;
     dma_data.mode = 0x0;
@@ -79,13 +73,18 @@ static inline s32 _loadIopModuleFromEEBuffer(s32 data, s32 module_size, s32 argc
     while (-0x1 < sceSifDmaStat(queue_id))
         ;
 
-    LoaderSysPrintf(GSTR(D_00136A48, "ldsys: _loadIopModuleFromEEBuffer: senddma: finish\n"));
+    LoaderSysPrintf("ldsys: _loadIopModuleFromEEBuffer: senddma: finish\n");
     stat = sceSifLoadModuleBuffer(module_addr, argc, argp);
     sceSifFreeSysMemory(module_addr);
     return stat;
 }
 
-extern const char D_00136AC0[];
+void setNewIopIdentifier(const char *newIdentifier)
+{
+    LoaderSysPrintf("ldsys: setNewIopIdentifier: set new iop identifier \"%s\" at #%d\n", newIdentifier, D_0013A108);
+    strncpy(D_0013CD10[D_0013A108++], newIdentifier, strlen(newIdentifier));
+}
+
 s32 LoaderSysLoadIopModuleFromEEBuffer(const char *module_ident, s32 data, s32 module_size, s32 argc, const char *argp)
 {
     s32 stat;
@@ -99,12 +98,12 @@ s32 LoaderSysLoadIopModuleFromEEBuffer(const char *module_ident, s32 data, s32 m
             stat = _loadIopModuleFromEEBuffer(data, module_size, argc, argp);
             if (-0x1 < stat)
             {
-                __inlined_setNewIopIdentifier(module_ident);
+                setNewIopIdentifier(module_ident);
             }
         }
         else
         {
-            LoaderSysPrintf(GSTR(D_00136AC0, "ldsys: LoaderSysLoadIopModuleFromEEBuffer: iop identifier \"%s\" exists at #%d (load skipped).\n"), module_ident, module_id);
+            LoaderSysPrintf("ldsys: LoaderSysLoadIopModuleFromEEBuffer: iop identifier \"%s\" exists at #%d (load skipped).\n", module_ident, module_id);
             return 0x0;
         }
     }
@@ -115,14 +114,64 @@ s32 LoaderSysLoadIopModuleFromEEBuffer(const char *module_ident, s32 data, s32 m
     return stat;
 }
 
+void LoaderSysDumpIopModuleIdentifiers(void)
+{
+    s32 i;
+
+    LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: dump list of iop identifiers\n");
+    LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: ----------------------------\n");
+
+    for (i = 0; i < D_0013A108; i++)
+    {
+        LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: \t% 2d: \"%s\"\n", i, D_0013CD10[i]);
+    }
+
+    LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: ----------------------------\n");
+}
+
+s32 LoaderSysGetMemoryInfo(memory_info *info)
+{
+    u32 stack_size;
+    u32 heap_size;
+
+    info->unk18 = (void *)D_00139F00;
+
+    info->stack_base = (void *)LoaderSysGetStackBase();
+    stack_size = LoaderSysGetStackSize();
+    info->heap_base = (void *)LoaderSysGetHeapBase();
+    heap_size = LoaderSysGetHeapSize();
+
+    info->module_stack_base = info->module_stack_end = (void *)D_0013A114;
+    info->module_stack_end += D_00131D00.stack_size;
+    info->stack_base2 = info->stack_base;
+    info->stack_end = info->stack_base + stack_size;
+    info->heap_end = info->heap_base + heap_size;
+    info->module_info = D_00131D00;
+
+    return 1;
+}
+
 s32 LoaderSysCheckCDBootMode()
 {
     return 2;
 }
-extern const char D_0013A0F8[]; // "%s", .sdata
-void LoaderSysPutString(char *string)
+
+void LoaderSysSendAbort(void)
 {
-    printf(GSTR(D_0013A0F8, "%s"), string);
+}
+
+char *checkHookDesc(char *hook_desc)
+{
+    s32 hasDiff;
+    char *desc;
+
+    hasDiff = strncmp(hook_desc, "host0:", 6);
+    desc = hook_desc + 6;
+    if (hasDiff == 0)
+    {
+        return desc;
+    }
+    return NULL;
 }
 
 s32 LoaderSysFOpen(const char *name, s32 flags, s32 mode)
@@ -187,78 +236,4 @@ s32 LoaderSysGetstat(const char *name, struct sce_stat *buf)
 s32 LoaderSysChstat(const char *name, struct sce_stat *buf, u32 cbit)
 {
     return sceChstat(name, buf, cbit);
-}
-
-extern s32 D_00139F00;
-s32 LoaderSysGetMemoryInfo(memory_info *info)
-{
-    u32 stack_size;
-    u32 heap_size;
-
-    info->unk18 = (void *)D_00139F00;
-
-    info->stack_base = (void *)LoaderSysGetStackBase();
-    stack_size = LoaderSysGetStackSize();
-    info->heap_base = (void *)LoaderSysGetHeapBase();
-    heap_size = LoaderSysGetHeapSize();
-
-    info->module_stack_base = info->module_stack_end = (void *)D_0013A114;
-    info->module_stack_end += D_00131D00.stack_size;
-    info->stack_base2 = info->stack_base;
-    info->stack_end = info->stack_base + stack_size;
-    info->heap_end = info->heap_base + heap_size;
-    info->module_info = D_00131D00;
-
-    return 1;
-}
-
-INCLUDE_RODATA("asm/nonmatchings/os/loaderSysFileIO", D_00136A00);
-
-INCLUDE_RODATA("asm/nonmatchings/os/loaderSysFileIO", D_00136A48);
-
-INCLUDE_RODATA("asm/nonmatchings/os/loaderSysFileIO", D_00136A80);
-
-INCLUDE_RODATA("asm/nonmatchings/os/loaderSysFileIO", D_00136AC0);
-
-void LoaderSysDumpIopModuleIdentifiers(void)
-{
-    s32 i;
-
-    LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: dump list of iop identifiers\n");
-    LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: ----------------------------\n");
-
-    for (i = 0; i < D_0013A108; i++)
-    {
-        LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: \t% 2d: \"%s\"\n", i, D_0013CD10[i]);
-    }
-
-    LoaderSysPrintf("ldsys: LoaderSysDumpIopModuleIdentifiers: ----------------------------\n");
-}
-
-void LoaderSysSendAbort(void)
-{
-}
-
-void LoaderSysFlushPrint(void)
-{
-}
-
-char *checkHookDesc(char *hook_desc)
-{
-    s32 hasDiff;
-    char *desc;
-
-    hasDiff = strncmp(hook_desc, GSTR(D_0013A100, "host0:"), 6);
-    desc = hook_desc + 6;
-    if (hasDiff == 0)
-    {
-        return desc;
-    }
-    return NULL;
-}
-
-void setNewIopIdentifier(const char *newIdentifier)
-{
-    LoaderSysPrintf(D_00136A80, newIdentifier, D_0013A108);
-    strncpy(D_0013CD10[D_0013A108++], newIdentifier, strlen(newIdentifier));
 }
