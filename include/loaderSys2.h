55ba1ea16acddc8bf18bd79993d1550cf32c5031
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2025-03-13 10:48:43 -0500

    Match loaderSys2.c at -O3

--- include/loaderSys2.h
+++ include/loaderSys2.h
@@ -2,48 +2,13 @@
 #define LOADERSYS2_H
 
 #include "common.h"
-#include "fl_xfftype.h"
-#include "gcc/string.h"
-#include "include_asm.h"
 #include "sdk/ee/eekernel.h"
-#include "sdk/ee/sifdev.h"
-#include "sdk/ee/sifrpc.h"
-
-#define ANSI_RED "\x1b[31m"
-#define ANSI_GREEN "\x1b[32m"
-#define ANSI_YELLOW "\x1b[33m"
-#define ANSI_BLUE "\x1b[34m"
-#define ANSI_MAGENTA "\x1b[35m"
-#define ANSI_CYAN "\x1b[36m"
-#define ANSI_WHITE "\x1b[37m"
-#define ANSI_RESET "\x1b[m"
 
 struct unk
 {
     s32 unk0;
     s32 unk4;
 };
-
-void LoaderSysDeleteAllExternalIntcHandler(void);
-void LoaderSysDeleteAllExternalSema(void);
-void LoaderSysExecuteRecoveryFirstProcess(void);
-void LoaderSysEntryExternalIntcHandlerList(s32 param_1, s32 param_2);
-void LoaderSysEntryExternalThreadList(s32 thread_id);
-void LoaderSysEntryExternalSemaList(s32 arg1);
-s32 LoaderSysEntryExternalIopMemoryList(s32 iopmem_id);
-s32 LoaderSysDeleteExternalIntcHandlerList(s32 arg);
-s32 LoaderSysDeleteExternalThreadList(s32 threadId);
-s32 LoaderSysDeleteExternalSemaList(s32 semaId);
-s32 LoaderSysDeleteExternalIopMemoryList(s32 iopmem_id);
-void LoaderSysInitExternalIntcHandlerList(void);
-void LoaderSysInitExternalSemaList(void);
-void LoaderSysInitExternalThreadList(void);
-void LoaderSysExternalThreadListCallBack(void (*fun_ptr)(s32));
-void LoaderSysChangeExternalThreadPriorityExceptMe(s32 priority);
-void LoaderSysDeleteAllExternalThread(void);
-void LoaderSysDeleteAllExternalThreadExceptMe(void);
-void LoaderSysInitExternalIopMemoryList(void);
-void LoaderSysDeleteAllExternalIopMemory(void);
 
 #define SEMAPHORE_LIST D_0013BD10
 #define THREAD_LIST D_0013B910
@@ -62,81 +27,30 @@ extern s32 D_0013A110;
 extern s32 D_0013A184;
 extern s32 D_0013A114;
 
-// sdata externs
-extern const char D_00131DB8[]; // "XFF2"
-extern const char D_0013A118[]; // "\"%s\""
-extern const char D_0013A120[]; // "... "
-extern const char D_0013A128[]; // "Done.\n"
+void LoaderSysEntryExternalIntcHandlerList(s32 param_1, s32 param_2);
+void LoaderSysEntryExternalThreadList(s32 thread_id);
+void LoaderSysEntryExternalSemaList(s32 arg1);
+s32 LoaderSysEntryExternalIopMemoryList(s32 iopmem_id);
 
-// TODO: merge these
-static inline void __inlined_LoaderSysChangeExternalThreadPriorityExceptMe(s32 priority)
-{
-    s32 i;
-    s32 threadId;
+s32 LoaderSysDeleteExternalIntcHandlerList(s32 arg);
+s32 LoaderSysDeleteExternalThreadList(s32 threadId);
+s32 LoaderSysDeleteExternalSemaList(s32 semaId);
+s32 LoaderSysDeleteExternalIopMemoryList(s32 iopmem_id);
 
-    threadId = GetThreadId();
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if ((THREAD_LIST[i] != threadId) && (-1 < THREAD_LIST[i]))
-        {
-            ChangeThreadPriority(THREAD_LIST[i], priority);
-        }
-    }
-    return;
-}
+void LoaderSysInitExternalIntcHandlerList(void);
+void LoaderSysInitExternalSemaList(void);
+void LoaderSysInitExternalThreadList(void);
+void LoaderSysExternalThreadListCallBack(void (*fun_ptr)(s32));
+void LoaderSysChangeExternalThreadPriorityExceptMe(s32 priority);
 
-static inline void __inlined_LoaderSysDeleteAllExternalThreadExceptMe(void)
-{
-    s32 threadId;
-    s32 i;
+void LoaderSysDeleteAllExternalThread(void);
+void LoaderSysDeleteAllExternalThreadExceptMe(void);
+void LoaderSysDeleteAllExternalIntcHandler(void);
+void LoaderSysDeleteAllExternalSema(void);
 
-    threadId = GetThreadId();
+void LoaderSysInitExternalIopMemoryList(void);
+void LoaderSysDeleteAllExternalIopMemory(void);
 
-    for (i = 0; i < MAX_THREADS; i++)
-    {
-        if ((THREAD_LIST[i] != threadId) && (THREAD_LIST[i] >= 0))
-        {
-            TerminateThread(THREAD_LIST[i]);
-            DeleteThread(THREAD_LIST[i]);
-            THREAD_LIST[i] = -1;
-        }
-    }
-}
-
-static inline void __inlined_LoaderSysDeleteAllExternalIopMemory(void)
-{
-    s32 i;
-
-    sceSifInitRpc(0);
-    sceSifInitIopHeap();
-
-    for (i = 0; i < IOP_MEM_LIST_LEN; i++)
-    {
-        if (IOP_MEMORY_LIST[i] != 0)
-        {
-            sceSifFreeIopHeap((void *)IOP_MEMORY_LIST[i]);
-            IOP_MEMORY_LIST[i] = 0;
-        }
-    }
-}
-
-static inline void __inlined_LoaderSysInitExternalSemaList(void)
-{
-    s32 i;
-    for (i = 0; i < MAX_SEMAPHORES; i++)
-    {
-        SEMAPHORE_LIST[i] = -1;
-    }
-}
-
-static inline void __inlined_LoaderSysInitExternalIntcHandlerList(void)
-{
-    s32 i;
-
-    for (i = 0; i < MAX_INTC_HANDLERS; i++)
-    {
-        INTC_HANDLER_LIST[i].unk0 = INTC_HANDLER_LIST[i].unk4 = -1;
-    }
-}
+void LoaderSysExecuteRecoveryFirstProcess(void);
 
 #endif /* LOADERSYS2_H */
--- src/os/loaderSys2.c
+++ src/os/loaderSys2.c
@@ -1,85 +1,10 @@
 #include "loaderSys2.h"
 #include "common.h"
-#include "fl_xfftype.h"
-#include "gcc/string.h"
-#include "putString.h"
-#include "regnames.h"
-#include "sdk/ee/eekernel.h"
-#include "sdk/ee/sif.h"
+#include "loaderSysFileIO.h"
 #include "sdk/ee/sifdev.h"
+#include "sdk/ee/sifrpc.h"
 
-void LoaderSysDeleteAllExternalIntcHandler(void)
-{
-    s32 i;
-
-    for (i = 0; i < 0x100; i++)
-    {
-        if (D_0013C110[i].unk4 >= 0)
-        {
-            DisableIntc(D_0013C110[i].unk0);
-            RemoveIntcHandler(D_0013C110[i].unk0, D_0013C110[i].unk4);
-            D_0013C110[i].unk0 = D_0013C110[i].unk4 = -1;
-        }
-    }
-
-    __inlined_LoaderSysInitExternalIntcHandlerList();
-}
-
-extern u32 D_00132080[3];
-static inline s32 LoaderSysCheckSemaAttribueList(s32 sema_id)
-{
-    struct SemaParam sparam;
-    s32 i;
-
-    if (ReferSemaStatus(sema_id, &sparam) == sema_id)
-    {
-        for (i = 0; i < 3; i++)
-        {
-            if (D_00132080[i] == sparam.attr)
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
-void LoaderSysDeleteAllExternalSema(void)
-{
-    s32 i;
-
-    for (i = 0; i < 0x100; i++)
-    {
-        if (SEMAPHORE_LIST[i] >= 0)
-        {
-            DeleteSema(SEMAPHORE_LIST[i]);
-            SEMAPHORE_LIST[i] = -1;
-        }
-    }
-
-    __inlined_LoaderSysInitExternalSemaList();
-
-    for (i = 0; i < 0x100; i++)
-    {
-        if (LoaderSysCheckSemaAttribueList(i) != 0)
-        {
-            DeleteSema(i);
-        }
-    }
-}
-
-void LoaderSysExecuteRecoveryFirstProcess(void)
-{
-    ChangeThreadPriority(GetThreadId(), 1);
-    LoaderSysDeleteAllExternalIntcHandler();
-    __inlined_LoaderSysChangeExternalThreadPriorityExceptMe(0x7F);
-    __inlined_LoaderSysDeleteAllExternalThreadExceptMe();
-    LoaderSysDeleteAllExternalSema();
-    LoaderSysFlushPrint();
-    __inlined_LoaderSysDeleteAllExternalIopMemory();
-}
-
-static inline s32 getStA()
+static inline s32 FindIntcHandlerIndex()
 {
     s32 i;
     for (i = 0; i < MAX_INTC_HANDLERS; i++)
@@ -88,13 +13,6 @@ static inline s32 getStA()
             return i;
     }
     return -1;
-}
-
-void LoaderSysEntryExternalIntcHandlerList(s32 param_1, s32 param_2)
-{
-    s32 i = getStA();
-    INTC_HANDLER_LIST[i].unk4 = param_1;
-    INTC_HANDLER_LIST[i].unk0 = param_2;
 }
 
 static inline s32 FindThreadIndex(void)
@@ -110,11 +28,6 @@ static inline s32 FindThreadIndex(void)
     return -1;
 }
 
-void LoaderSysEntryExternalThreadList(s32 thread_id)
-{
-    THREAD_LIST[FindThreadIndex()] = thread_id;
-}
-
 static inline s32 FindSemaIndex(void)
 {
     s32 i;
@@ -126,11 +39,6 @@ static inline s32 FindSemaIndex(void)
         }
     }
     return -1;
-}
-
-void LoaderSysEntryExternalSemaList(s32 arg1)
-{
-    SEMAPHORE_LIST[FindSemaIndex()] = arg1;
 }
 
 static inline s32 FindIopMemoryIndex(s32 iopmem_id)
@@ -145,6 +53,23 @@ static inline s32 FindIopMemoryIndex(s32 iopmem_id)
         }
     }
     return -1;
+}
+
+void LoaderSysEntryExternalIntcHandlerList(s32 param_1, s32 param_2)
+{
+    s32 i = FindIntcHandlerIndex();
+    INTC_HANDLER_LIST[i].unk4 = param_1;
+    INTC_HANDLER_LIST[i].unk0 = param_2;
+}
+
+void LoaderSysEntryExternalThreadList(s32 thread_id)
+{
+    THREAD_LIST[FindThreadIndex()] = thread_id;
+}
+
+void LoaderSysEntryExternalSemaList(s32 arg1)
+{
+    SEMAPHORE_LIST[FindSemaIndex()] = arg1;
 }
 
 s32 LoaderSysEntryExternalIopMemoryList(s32 iopmem_id)
@@ -221,12 +146,21 @@ s32 LoaderSysDeleteExternalIopMemoryList(s32 iopmem_id)
 
 void LoaderSysInitExternalIntcHandlerList(void)
 {
-    __inlined_LoaderSysInitExternalIntcHandlerList();
+    s32 i;
+
+    for (i = 0; i < MAX_INTC_HANDLERS; i++)
+    {
+        INTC_HANDLER_LIST[i].unk0 = INTC_HANDLER_LIST[i].unk4 = -1;
+    }
 }
 
 void LoaderSysInitExternalSemaList(void)
 {
-    __inlined_LoaderSysInitExternalSemaList();
+    s32 i;
+    for (i = 0; i < MAX_SEMAPHORES; i++)
+    {
+        SEMAPHORE_LIST[i] = -1;
+    }
 }
 
 void LoaderSysInitExternalThreadList(void)
@@ -252,7 +186,18 @@ void LoaderSysExternalThreadListCallBack(void (*fun_ptr)(s32))
 
 void LoaderSysChangeExternalThreadPriorityExceptMe(s32 priority)
 {
-    __inlined_LoaderSysChangeExternalThreadPriorityExceptMe(priority);
+    s32 i;
+    s32 threadId;
+
+    threadId = GetThreadId();
+    for (i = 0; i < MAX_THREADS; i++)
+    {
+        if ((THREAD_LIST[i] != threadId) && (-1 < THREAD_LIST[i]))
+        {
+            ChangeThreadPriority(THREAD_LIST[i], priority);
+        }
+    }
+    return;
 }
 
 void LoaderSysDeleteAllExternalThread(void)
@@ -272,7 +217,80 @@ void LoaderSysDeleteAllExternalThread(void)
 
 void LoaderSysDeleteAllExternalThreadExceptMe(void)
 {
-    __inlined_LoaderSysDeleteAllExternalThreadExceptMe();
+    s32 threadId;
+    s32 i;
+
+    threadId = GetThreadId();
+
+    for (i = 0; i < MAX_THREADS; i++)
+    {
+        if ((THREAD_LIST[i] != threadId) && (THREAD_LIST[i] >= 0))
+        {
+            TerminateThread(THREAD_LIST[i]);
+            DeleteThread(THREAD_LIST[i]);
+            THREAD_LIST[i] = -1;
+        }
+    }
+}
+
+void LoaderSysDeleteAllExternalIntcHandler(void)
+{
+    s32 i;
+
+    for (i = 0; i < 0x100; i++)
+    {
+        if (D_0013C110[i].unk4 >= 0)
+        {
+            DisableIntc(D_0013C110[i].unk0);
+            RemoveIntcHandler(D_0013C110[i].unk0, D_0013C110[i].unk4);
+            D_0013C110[i].unk0 = D_0013C110[i].unk4 = -1;
+        }
+    }
+
+    LoaderSysInitExternalIntcHandlerList();
+}
+
+extern u32 D_00132080[3];
+static inline s32 LoaderSysCheckSemaAttribueList(s32 sema_id)
+{
+    struct SemaParam sparam;
+    s32 i;
+
+    if (ReferSemaStatus(sema_id, &sparam) == sema_id)
+    {
+        for (i = 0; i < 3; i++)
+        {
+            if (D_00132080[i] == sparam.attr)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void LoaderSysDeleteAllExternalSema(void)
+{
+    s32 i;
+
+    for (i = 0; i < 0x100; i++)
+    {
+        if (SEMAPHORE_LIST[i] >= 0)
+        {
+            DeleteSema(SEMAPHORE_LIST[i]);
+            SEMAPHORE_LIST[i] = -1;
+        }
+    }
+
+    LoaderSysInitExternalSemaList();
+
+    for (i = 0; i < 0x100; i++)
+    {
+        if (LoaderSysCheckSemaAttribueList(i) != 0)
+        {
+            DeleteSema(i);
+        }
+    }
 }
 
 void LoaderSysInitExternalIopMemoryList(void)
@@ -287,5 +305,28 @@ void LoaderSysInitExternalIopMemoryList(void)
 
 void LoaderSysDeleteAllExternalIopMemory(void)
 {
-    __inlined_LoaderSysDeleteAllExternalIopMemory();
+    s32 i;
+
+    sceSifInitRpc(0);
+    sceSifInitIopHeap();
+
+    for (i = 0; i < IOP_MEM_LIST_LEN; i++)
+    {
+        if (IOP_MEMORY_LIST[i] != 0)
+        {
+            sceSifFreeIopHeap((void *)IOP_MEMORY_LIST[i]);
+            IOP_MEMORY_LIST[i] = 0;
+        }
+    }
+}
+
+void LoaderSysExecuteRecoveryFirstProcess(void)
+{
+    ChangeThreadPriority(GetThreadId(), 1);
+    LoaderSysDeleteAllExternalIntcHandler();
+    LoaderSysChangeExternalThreadPriorityExceptMe(0x7F);
+    LoaderSysDeleteAllExternalThreadExceptMe();
+    LoaderSysDeleteAllExternalSema();
+    LoaderSysFlushPrint();
+    LoaderSysDeleteAllExternalIopMemory();
 }
